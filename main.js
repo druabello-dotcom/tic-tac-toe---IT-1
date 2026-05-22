13cdf1c145be78369f773297dcc02794a6e2cee4
Dru C. Romano Abello <druabello@gmail.com>
2026-05-22 23:02:20 +0200

    feat: add event listeners to all tiles

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 let turnDecider = 'X';
 let turnCounter = 0;
 let gameActive = true;
+let boardState = [];
 let markIdentifer = {
 	x: 1,
 	o: 2
 }
-let boardState = [];
 
 // create board
 let board = document.getElementById("game-board");
 for (let i = 0; i < 9; i++) {
 	let tile = document.createElement("div");
 	tile.classList.add("tile");
+	tile.setAttribute("id", i);
 
 	board.appendChild(tile);
-	boardState.push(i);
+	boardState.push(0);
+}
+
+let grid = Array.from(document.querySelectorAll(".tile"));
+for (let i = 0; i < 9; i++) {
+	grid[i].addEventListener("click", registerMark);
 }
