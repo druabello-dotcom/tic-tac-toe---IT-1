9b64d4798976597359671295ef42579c8fd232a5
Dru C. Romano Abello <druabello@gmail.com>
2026-05-22 20:55:09 +0200

    feat: set initial variables and game state

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+let turnDecider = 'X';
+let turnCounter = 0;
+let gameActive = true;
+let markIdentifer = {
+	x: 1,
+	o: 2
+}
+let boardState = [];
+
 // create board
 let board = document.getElementById("game-board");
 for (let i = 0; i < 9; i++) {
 	let tile = document.createElement("div");
 	tile.classList.add("tile");
+
 	board.appendChild(tile);
+	boardState.push(i);
 }
