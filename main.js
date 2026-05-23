acb11e55b7fd0a949b4f52ac2c4199dfd81710bb
Dru C. Romano Abello <druabello@gmail.com>
2026-05-23 19:02:39 +0200

    feat: make check-game-state function

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ let markIdentifier = {
 	O: 2
 }
 
+//—————————————————————————————————————————————————————
+
 // create board
 let board = document.getElementById("game-board");
 let playerTurnIndicator = document.getElementById("player-turn-indicator");
@@ -24,6 +26,8 @@ for (let i = 0; i < 9; i++) {
 	grid[i].addEventListener("click", registerMark);
 }
 
+//—————————————————————————————————————————————————————
+
 // game will react when tile is clicked
 function registerMark(event) {
 	let selectedTileNumber = Number(event.target.id);
@@ -32,7 +36,8 @@ function registerMark(event) {
 	boardState[selectedTileNumber] = markIdentifier[turnDecider];
 	turnCounter++;
 	displayMark(selectedTileNumber);
-	registerTurn(turnCounter);
+	registerTurn();
+	checkGameState();
 }
 
 function registerTurn() {
@@ -55,3 +60,60 @@ function displayMark(stn) {
 	markTile.textContent = turnDecider;
 	markTile.classList.add(turnDeciderClass);
 }
+
+function checkGameState() {
+	//check vertcally
+	for (let i = 0; i < 3; i++) {
+		let initMark = boardState[i];
+		let identicalCounter = 0;
+		for (let t = i; t <= (i + 6); t+=3) {
+			if (boardState[t] === initMark && boardState[t]) identicalCounter++;
+		}
+
+		if (identicalCounter === 3) {
+			declareTerminalState();
+			return;
+		}
+	}
+
+	//check horizontally
+	for (let i = 0; i <= 6; i+=3) {
+		let initMark = boardState[i];
+		let identicalCounter = 0;
+		for (let t = i; t < (i + 3); t++) {
+			if (boardState[t] === initMark && boardState[t]) identicalCounter++;
+		}
+
+		if (identicalCounter === 3) {
+			declareTerminalState();
+			return;
+		}
+	}
+
+	//check diagonally
+	let initMark = boardState[0];
+	let identicalCounter = 0;
+	for (let i = 0; i < 9; i+=4) {
+		if (boardState[i] === initMark && boardState[i]) identicalCounter++;
+	}
+	if (identicalCounter === 3) {
+		declareTerminalState();
+		return;
+	}
+
+	initMark = boardState[2];
+	identicalCounter = 0;
+	for (let i = 2; i <= 6; i+=2) {
+		if (boardState[i] === initMark && boardState[i]) identicalCounter++;
+	}
+	if (identicalCounter === 3) {
+		declareTerminalState();
+		return;
+	}
+}
+
+function declareTerminalState() {
+	document.body.style.backgroundColor = "red";
+}
+
+//—————————————————————————————————————————————————————
